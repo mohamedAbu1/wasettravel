baa514736bdfa036e5cdeee6e57304fd7cf742f0
Mohamed Abu <mohamedahmed33m11@gmail.com>
2026-03-26 12:47:45 +0200

    Initial commit - ??? ????? SolDelNilo

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-
   images: {
-    unoptimized: true, // 🔥 تعطيل Image Optimization لمنع 402 من Vercel
-
     domains: [
-      "dxpbyrcbklqrjlytmkum.supabase.co", // دومين Supabase الأول
-      "pqeliprhapbghcczgyru.supabase.co", // دومين Supabase الثاني
+      "dxpbyrcbklqrjlytmkum.supabase.co",
+      "pqeliprhapbghcczgyru.supabase.co",
     ],
   },
 };

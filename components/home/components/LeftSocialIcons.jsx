f9d1db45a68c8c8e28c543344c5fc29fb338222d
Mohamed Abu <mohamedahmed33m11@gmail.com>
2026-05-03 20:52:18 +0300

    رسالة التعديل

--- components/home/components/LeftSocialIcons.jsx
+++ components/home/components/LeftSocialIcons.jsx
@@ -22,7 +22,7 @@ const LeftSocialIcons = () => {
       initial={{ x: -100, opacity: 0 }}
       animate={{ x: 0, opacity: 1 }}
       transition={{ duration: 1, delay: 0.7 }}
-      className="absolute left-[10%] bottom-0 lg:top-[55%] -translate-y-1/2 flex flex-row lg:flex-col gap-6 z-30"
+      className="absolute left-[30px] bottom-0 lg:top-[55%] -translate-y-1/2 flex flex-row lg:flex-col gap-6 z-30"
     >
       {socialLinks.map(({ Icon, url }, i) => (
         <motion.a

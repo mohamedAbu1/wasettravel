515f0b174b0cb4de1f2432414f5cffafb8597cbd
Mohamed Abu <mohamedahmed33m11@gmail.com>
2026-06-25 18:59:58 +0300

    إضافة الملفات الأساسية للمشروع

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
   images: {
     domains: [
       "dxpbyrcbklqrjlytmkum.supabase.co", // أضف دومين Supabase هنا
-      "bsrlydzntfpuyxcqwjpl.supabase.co", // لو عندك أكثر من مشروع أو bucket
+      "lkwlrezhuxercfvtjiiw.supabase.co", // لو عندك أكثر من مشروع أو bucket
     ],
     qualities: [75, 85, 100], // ✅ لتفادي التحذير في Next.js 16
   },
